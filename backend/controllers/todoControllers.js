59c57dfbc0300ae687b9c43e2852de032d7e40f3
Lina <lina@creativehandles.com>
2021-02-26 13:26:32 +0300

    todo with add and get method

--- backend/controllers/todoControllers.js
+++ backend/controllers/todoControllers.js
@@ -11,9 +11,13 @@ module.exports.getTodo= async(req,res)=>{
 }
 module.exports.postTodo=async(req,res)=>{
     try {
-        console.log(req.body)
+        const{item,id}=req.body
+        await Todo.create({item,id})
+        res.json({messahe:"Todo created succesfully"})
+        console.log("Todo created succesfully")
     } catch (error) {
-        
+        console.log("Todo not created")
+        console.log(error)
     }
     
 }
